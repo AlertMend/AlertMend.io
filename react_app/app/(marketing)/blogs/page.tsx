9c20d6830b29b75ceb0e11bce399aceea394d11f
MukulVSC <mukul@valleysc.in>
2025-07-18 13:45:49 +0530

    minor changes

--- react_app/app/(marketing)/blogs/page.tsx
+++ react_app/app/(marketing)/blogs/page.tsx
@@ -14,9 +14,9 @@ import { format } from "date-fns";
 export const metadata: Metadata = {
   title: "Blog | AlertMend ",
   description:
-    "Everything AI is a platform that provides a wide range of AI tools and services to help you stay on top of your business. Generate images, text and everything else that you need to get your business off the ground.",
+    "",
   openGraph: {
-    images: ["https://ai-saas-template-aceternity.vercel.app/banner.png"],
+    images: ["/img/alertmend_logo.jpg"],
   },
 };
 
--- react_app/app/(marketing)/case-studies/page.tsx
+++ react_app/app/(marketing)/case-studies/page.tsx
@@ -14,9 +14,9 @@ import { CaseStudyCard } from "@/components/case-study-card";
 export const metadata: Metadata = {
   title: "CaseStudy | AlertMend ",
   description:
-    "Everything AI is a platform that provides a wide range of AI tools and services to help you stay on top of your business. Generate images, text and everything else that you need to get your business off the ground.",
+    "",
   openGraph: {
-    images: ["https://ai-saas-template-aceternity.vercel.app/banner.png"],
+    images: ["/img/alertmend_logo.jpg"],
   },
 };
 
