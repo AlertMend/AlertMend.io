92eec23cd0f07e61ab5db55bb50a8cfa2d021e76
kuldeep soni <kuldeep@futureapp.in>
2025-08-27 15:31:33 +0530

    Explore playground

--- react_app/components/hero.tsx
+++ react_app/components/hero.tsx
@@ -60,12 +60,22 @@ export const Hero = () => {
           showStars
         /> */}
         <div className="flex items-center gap-4 justify-center my-10 relative z-10">
-          <Button className="flex space-x-2 items-center group !text-lg">
+          <a href="https://demo.alertmend.io/playground" target="_blank"
+          rel="noopener noreferrer"
+          className="text-inherit no-underline">
+           <Button variant="muted" className="flex space-x-2 items-center group !text-lg">
+              Explore Playground
+            </Button>
+          </a>
           <a href="https://calendly.com/hello-alertmend/30min" target="_blank"
           rel="noopener noreferrer"
-          className="text-inherit no-underline">Book a demo</a>{" "}
+          className="text-inherit no-underline">
+          <Button className="flex space-x-2 items-center gap-2 group !text-lg">
+            Book a demo
+            {" "}
             <HiArrowRight className="text-black group-hover:translate-x-1 stroke-[1px] h-3 w-3 mt-0.5 transition-transform duration-200" />
           </Button>
+          </a>
         </div>
       </Container>
       <div className="flex  items-center justify-center relative p-2 md:p-20 cursor-pointer md:-mt-20">
